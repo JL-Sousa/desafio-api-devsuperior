2672e722329589f756b17f59210a46349a78d635
Jonyelson Lopes <jonyelson20@gmail.com>
2022-11-25 20:57:27 -0300

    customer resource

--- api/src/main/java/com/tecodev/api/repository/ClientRepository.java
+++ api/src/main/java/com/tecodev/api/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.tecodev.api.repository;
 
 import com.tecodev.api.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
