27c38aca3ee932ec5c126793aceebf470e899b95
Jonyelson Lopes <jonyelson20@gmail.com>
2022-11-25 21:06:35 -0300

    Seeding base dados

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -4,12 +4,22 @@ INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
 'Joao Carlos', '11122233345', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1);
 INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
 'Ezequias Sousa', '09876543256', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Maria Elenice', '05920923456', 6000.00,  TIMESTAMP WITH TIME ZONE '1970-07-14T10:00:00Z', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Juan Pablo', '10022233345', 1500.00,  TIMESTAMP WITH TIME ZONE '2010-07-14T10:00:00Z', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Jancy Cleyton', '09822543256', 7000.00,  TIMESTAMP WITH TIME ZONE '1988-07-14T10:00:00Z', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Alice', '05920923456', 5000.00,  TIMESTAMP WITH TIME ZONE '1993-07-14T10:00:00Z', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Thomas Muller', '11122233345', 20000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 4);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Daniela Campos', '09876773256', 4000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 0);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Vinicios Lopes', '05911923456', 3000.00,  TIMESTAMP WITH TIME ZONE '2022-05-14T10:00:00Z', 1);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Carlos Miguel', '13322233345', 5000.00,  TIMESTAMP WITH TIME ZONE '2021-09-14T10:00:00Z', 3);
+INSERT INTO tb_clients (name, cpf, income, birth_date, children) VALUES (
+'Geicy Nayra', '09876543356', 1000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 2);
 
---('Jonyelson', '56789043223', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1),
---('Jonyelson', '4355464774', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1),
---('Jonyelson', '4355464774', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1),
---('Jonyelson', '4355464774', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1),
---('Jonyelson', '4355464774', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1),
---('Jonyelson', '4355464774', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1),
---('Jonyelson', '4355464774', 2000.00,  TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 1)
---);
